930e874183ae74a613a94b2cf32f38e40bef969b
Rino Miguel Sagario Quijote <149220242+sagaKnight@users.noreply.github.com>
2024-04-14 16:17:55 +1000

    First Draft

--- science-island-webpages/src/App.js
+++ science-island-webpages/src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { Box, Typography, Grid, Button } from "@mui/material";
+import heroImage from "./images/heroImage.png"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        {/* Hero Section */}
+        <div>
+          <Box>
+              <Grid container spacing={8}>
+                <Grid item xs={12} sm={4} md={4}>
+                  <Typography variant="h3" >
+                     Science Island: Where Learning and Fun Collide!
+                  </Typography>
+                  <Typography variant="h6">
+                  Adventure, Discovery, and Learning Await! Join Forces with Global Experts to Experience an Immersive Educational Journey Like No Other!
+                  </Typography>
+                  <Button variant="contained" color="primary">
+                    Join Us
+                  </Button>
+                </Grid>
+                <Grid item xs={12} sm={8} md={8}>
+                  <img src={heroImage} alt="Hero Image" />
+                </Grid>
+              </Grid>
+          </Box>
+        </div>
     </div>
   );
 }
