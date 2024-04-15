c3bae3dfa5b58ccc110ea88b9dec8f2806830b42
Rino Miguel Sagario Quijote <149220242+sagaKnight@users.noreply.github.com>
2024-04-15 17:20:40 +1000

    Nav Links Testing

--- science-island-webpages/src/components/nav.js
+++ science-island-webpages/src/components/nav.js
@@ -1,16 +1,29 @@
 import React from 'react';
 import { AppBar, Toolbar, Typography } from '@mui/material';
-import Logo from '../images/logo.png'
+import Logo from '../images/logo.png';
+import { Link } from 'react-router-dom'; 
+import '../stylesheets/global.css'
 
 function Nav() {
   return (
     <div>
       <AppBar position="static">
         <Toolbar>
+          <Link to="/">
           <img src={Logo} style={{ maxWidth: '100px', maxHeight: '100px' }} />
+          </Link>
+          <Typography variant='h6'>
+            <Link to="/about" className='nav-link'>
+              About
+            </Link>
+          </Typography>
+          <Typography variant='h6'>
+            <Link to="/about" className='nav-link'>
+              This needs some work
+            </Link>
+          </Typography>
         </Toolbar>
       </AppBar>
-      {/* Other content of your app */}
   </div>
 
   );
