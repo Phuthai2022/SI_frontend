ec5a4f75785d1f6a550c7bd63339ef35e51b74cc
Rino Miguel Sagario Quijote <149220242+sagaKnight@users.noreply.github.com>
2024-04-15 15:30:19 +1000

    React Player + Video Section Working

--- science-island-webpages/src/App.js
+++ science-island-webpages/src/App.js
@@ -1,5 +1,6 @@
 import { Box, Typography, Grid, Button, Container } from "@mui/material";
-import heroImage from "./images/heroimage.png"
+import heroImage from "./images/heroimage.png";
+import ReactPlayer from 'react-player';
 //Importing stylesheet for page here...
 import "./stylesheets/homepage.css"
 import "./stylesheets/global.css"
@@ -13,32 +14,75 @@ function App() {
               <Grid container spacing={8}>
                 <Grid item xs={12} sm={12} md={5} lg={4} textAlign={"center"} alignContent={"center"}>
                   <Typography variant="h3" >
-                     Science Island: Where Learning and Fun Collide!
+                    Acclaimed Gamified Education Platform for Primary to PhD
                   </Typography>
                   <Typography variant="h6">
-                  Adventure, Discovery, and Learning Await! Join Forces with Global Experts to Experience an Immersive Educational Journey Like No Other!
+                  Over 100,000+ hours of research and development, Science Island offers captivating animated videos, games, quizzes, and experiments!
                   </Typography>
                   <Button variant="contained" color="primary">
-                    Join Us
+                    Play Now
                   </Button>
                 </Grid>
                 <Grid item xs={12} sm={12} md={7} lg={8}>
                   <img src={heroImage} alt="Hero Image" className="hero-image" />
-                  <figcaption style={{color: "red"}}>*To be replaced by HD image. Can be replaced by a slideshow also.</figcaption>
                 </Grid>
               </Grid>
         </Container>
+
+        {/* Should have an awards section here which scrolls through some stuff. Or, schools which scrolls through */}
+        <Container>
+          <Grid xs={12} lg={12} textAlign={"center"}>
+            <Typography variant="h2" color={"grey"}>Award/Universities Contribution Section Should Be Here. Logos Only. Pref Scrolling.</Typography>
+          </Grid>
+        </Container>
+
         {/* Video Section */}
-        <Container maxWidth="xl" className="container">
-          <Grid>
-            <Grid item xs={12} sm={12} md={12} lg={12} textAlign={"center"}>
+        <Container maxWidth="xl" className="container" >
+          <Grid container justifyContent={"center"} alignItems={"center"} display={"flex"} flexDirection={"column"}>
+            <Grid item xs={12} sm={12} md={12} lg={12} sx={{textAlign: "center"}} >
               <Typography variant="h3">
-                Introduction Video text text text
+                Introducing Science Island
+              </Typography>
+              <Typography variant="h6">
+                Calling All Curious Minds: Science Island Awaits! Perfect for Young Explorers and Future Scientists!
               </Typography>
             </Grid>
             <Grid item xs={12} sm={12} md={12} lg={12}>
-              
+              <ReactPlayer url="https://vimeo.com/210883372" controls className="react-player"/>
             </Grid>
+          </Grid>
+        </Container>
+
+        {/* Why Use Science Island Section */}
+        <Container maxWidth="xl" className="container">
+          <Grid container spacing={4} textAlign={"center"}>
+              <Grid item lg={12}>
+                <Typography variant="h3">
+                  Why Use Science Island?
+                </Typography>
+              </Grid>
+              <Grid item lg={3}>
+                <Box className="why-section-box">
+                  <h3>Inspire Lifelong Learning</h3>
+                  <img src=""></img>
+                  <p></p>
+                </Box>
+              </Grid>
+              <Grid item lg={3}>
+                <Box className="why-section-box">
+                  <h3>The</h3>
+                </Box>
+              </Grid>
+              <Grid item lg={3}>
+                <Box className="why-section-box">
+                  <h3>The</h3>
+                </Box>
+              </Grid>
+              <Grid item lg={3}>
+                <Box className="why-section-box">
+                  <h3>The</h3>
+                </Box>
+              </Grid>
           </Grid>
         </Container>
     </div>
